903c2bc2cc2032125b9f5692282e2bcc20e8af42
DeBraid <de.braid@gmail.com>
2014-09-18 11:31:31 -0400

    added new video

--- client/views/youtube.js
+++ client/views/youtube.js
@@ -4,7 +4,8 @@ Template.youtube.helpers({
       { 'title' : 'Introduction to Hamilton Mayoral Candidates 2014', 'url' : '//www.youtube.com/embed/xBsz6bucCNo'  },
       { 'title' : 'First 100 days in Office' , 'url' : '//www.youtube.com/embed/xhyzy8_VVDc' },
       { 'title' : 'Why do you want to be the Mayor of Hamilton?' , 'url' : '//www.youtube.com/embed/4yWPBk9C9ZQ' },
-      { 'title' : 'Hamilton\'s Greatest Economic Asset' , 'url' : '//www.youtube.com/embed/1J6UT9kR2Ao' }
+      { 'title' : 'Hamilton\'s Greatest Economic Asset' , 'url' : '//www.youtube.com/embed/1J6UT9kR2Ao' },
+      { 'title' : 'A vision of what Hamilton looks like in 2034' , 'url' : '//www.youtube.com/embed/R73PKPft1xo' }
     ];
   }
 }); 
